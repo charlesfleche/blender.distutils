8f4657f3e283aceda9fa2dd4d4673b61b97b9fa7
Charles Flèche <charles.fleche@free.fr>
2018-07-15 23:58:26 +0200

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='blender.distutils',
-    version='1.0.1',
+    version='1.0.2',
     description='Blender distutils addon',
     long_description=open('README.md').read(),
     url='https://github.com/charlesfleche/blender.distutils',
